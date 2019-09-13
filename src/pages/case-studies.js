c06c5bf66c8ad4ccf0ec2dc194f899b8f8274f4d
Stefan Crowhurst <stefan.crowhurst@prospectsoft.com>
2019-09-13 16:39:38 +0100

    End of day commit

---
+++ src/pages/case-studies.js
@@ -0,0 +1,121 @@
+import React from "react"
+import Layout from "../components/layout"
+
+import "./case-studies.scss"
+
+export default class CaseStudies extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <main>
+      <section class="section-hero" id="home">
+        <div class="content-container">
+          <div class="col-1">
+              <img src="https://via.placeholder.com/1000x250" />
+            </div>
+        </div>
+      </section>
+      <section class="section-about-us" id="zing-and-twilio">
+          <div class="content-container">
+            <div class="col-5">
+             <blockquote class="blockquote"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum odit, cumque, quae porro deleniti animi delectus explicabo unde magnam, omnis cum id. Esse optio, ducimus neque iure aliquid voluptatem! Iusto dignissimos eum mollitia beatae dolorum, a veniam delectus possimus? Dolores.</p></blockquote>
+             <p className="p-cent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque maiores unde illum tempora non corrupti eum tempore possimus, autem consectetur velit quod error minima sint, perspiciatis dolores quidem, nesciunt sed. Corporis, quod ab. Ut impedit corporis, dolores culpa cupiditate, obcaecati aliquam id officia quibusdam, iusto mollitia cum quis voluptatem. Quasi!</p>
+             <p className="p-cent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque maiores unde illum tempora non corrupti eum tempore possimus, autem consectetur velit quod error minima sint, perspiciatis dolores quidem, nesciunt sed. Corporis, quod ab. Ut impedit corporis, dolores culpa cupiditate, obcaecati aliquam id officia quibusdam, iusto mollitia cum quis voluptatem. Quasi!</p>
+            </div>
+          </div>
+        </section>
+      <section class="section-about-us" id="zing-and-twilio">
+        <div class="content-container">
+          <div class="col-5">
+            <h1>SIMPLIFi</h1>
+            <h2>Morbi porttitor dignissim elit at fermentum. Fusce iaculis eget quam id pharetra.</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo error ipsum cum iste numquam aliquam quod. Est, aliquam? Porro magni minima assumenda architecto vel, laboriosam temporibus excepturi libero culpa placeat vero ab veniam obcaecati quod quam mollitia voluptatum! Necessitatibus consequatur eos quas eius similique impedit voluptatibus officiis odio aliquam eaque porro modi unde quaerat nemo magni temporibus accusamus facilis fuga fugiat, officia ducimus autem. Nulla aliquam earum in enim quisquam consequatur magni natus doloribus omnis reprehenderit. Quaerat pariatur veniam sequi fugiat sit? Repellendus odit, sunt mollitia autem quas aut nisi laudantium est velit, cupiditate esse quasi asperiores atque ut animi.</p>
+          </div>
+          <div class="col-1">
+            <img src="https://via.placeholder.com/300x300" />
+          </div>
+        </div>
+      </section>
+      <section class="section-about-us" id="zing-and-twilio">
+          <div class="content-container">
+            <div class="col-1">
+              <img src="https://via.placeholder.com/300x300" />
+            </div>
+            <div class="col-5">
+              <h1>SIMPLIFi</h1>
+              <h2>Morbi porttitor dignissim elit at fermentum. Fusce iaculis eget quam id pharetra.</h2>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo error ipsum cum iste numquam aliquam quod. Est, aliquam? Porro magni minima assumenda architecto vel, laboriosam temporibus excepturi libero culpa placeat vero ab veniam obcaecati quod quam mollitia voluptatum! Necessitatibus consequatur eos quas eius similique impedit voluptatibus officiis odio aliquam eaque porro modi unde quaerat nemo magni temporibus accusamus facilis fuga fugiat, officia ducimus autem. Nulla aliquam earum in enim quisquam consequatur magni natus doloribus omnis reprehenderit. Quaerat pariatur veniam sequi fugiat sit? Repellendus odit, sunt mollitia autem quas aut nisi laudantium est velit, cupiditate esse quasi asperiores atque ut animi.</p>
+            </div>
+          </div>
+        </section>
+        <section class="section-what-we-do" id="what-we-do">
+        <div class="content-container">
+          <div class="content-header">
+            <h4 class="title-red">
+              A project for CET
+            </h4>
+            <h3 class="title">
+              Porttitor dignissim elit at fermentum.
+            </h3>
+            <h3 class="title">
+              Fusce iaculis eget quam id pharetra.
+            </h3>
+          </div>
+          <div className="exdiv">
+            <div class="row">
+                <div class="column">
+                <div class="card">
+                    <img src="https://via.placeholder.com/100" />
+                    <h4>Nam nec quam</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sequi asperiores suscipit at. Architecto, a.</p>
+                </div>
+                </div>
+            
+                <div class="column">
+                <div class="card">
+                    <img src="https://via.placeholder.com/100" />
+                    <h4>Nam nec quam</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sequi asperiores suscipit at. Architecto, a.</p>
+                </div>
+                </div>
+                
+                <div class="column">
+                <div class="card">
+                    <img src="https://via.placeholder.com/100" />
+                    <h4>Nam nec quam</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sequi asperiores suscipit at. Architecto, a.</p>
+                </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="column">
+                <div class="card">
+                    <img src="https://via.placeholder.com/100" />
+                    <h4>Nam nec quam</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sequi asperiores suscipit at. Architecto, a.</p>
+                </div>
+                </div>
+            
+                <div class="column">
+                <div class="card">
+                    <img src="https://via.placeholder.com/100" />
+                    <h4>Nam nec quam</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sequi asperiores suscipit at. Architecto, a.</p>
+                </div>
+                </div>
+                <div class="column">
+                <div class="card">
+                    <img src="https://via.placeholder.com/100" />
+                    <h4>Nam nec quam</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sequi asperiores suscipit at. Architecto, a.</p>
+                </div>
+                </div>
+            </div>
+        </div>
+        </div>
+      </section>
+    </main>
+    </Layout>
+    )
+  }
+}
