0d589d79b3a858d1f66a4d5137b9ad3243447126
Jordan Edmunds <jordan.edmunds@prospectsoft.com>
2019-10-04 16:37:40 +0100

    Made some pretty boxes.

--- src/pages/jordan-page.js
+++ src/pages/jordan-page.js
@@ -10,15 +10,24 @@ export default class JordanPage extends React.Component {
         <section className="section-hero jordan-hero" id="privacy">
           <div className="content-container">
             <div className="col-1">
-              <h1>Privacy Policy</h1>
+              <h1>Zing Resources</h1>
             </div>
           </div>
         </section>
         <section>
           <div className="content-container">
-            <div className="col-1">
-              <p>Hello World</p>
+            <div className="container-row">
+              <div className="row-column">Hi </div>
+              <div className="row-column">Hi to the right </div>
             </div>
+
+          </div>
+          <div className="content-container">
+            <div className="container-row">
+              <div className="row-column">Hi </div>
+              <div className="row-column">Hi to the right </div>
+            </div>
+
           </div>
         </section>
       </Layout>
