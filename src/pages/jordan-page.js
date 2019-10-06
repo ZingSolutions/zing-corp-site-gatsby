772682e11d4669d81708b0742345547bf4238636
Jordan Edmunds <jordan.edmunds@prospectsoft.com>
2019-10-06 18:26:42 +0100

    split into 3 rows, added image

--- src/pages/jordan-page.js
+++ src/pages/jordan-page.js
@@ -17,18 +17,61 @@ export default class JordanPage extends React.Component {
         <section>
           <div className="content-container">
             <div className="container-row">
-              <div className="row-column">Hi </div>
-              <div className="row-column">Hi to the right </div>
+              <div className="image-row-column">
+                <img
+                  src="/images/question.jpg"
+                  alt="Question"
+                  width="308px"
+                  height="200px"
+                />
+              </div>
+              <div className="row-column">
+                <h4>Resources</h4><br></br>
+                <h3>Lorum Ipsum blah blah blah</h3><br></br>
+                <p>Here is some more filler text.  This should look nice hopefully.</p>
+                <a href="#" class="call-to-action">Click Here</a>
+              </div>
             </div>
 
           </div>
           <div className="content-container">
             <div className="container-row">
-              <div className="row-column">Hi </div>
-              <div className="row-column">Hi to the right </div>
+              <div className="row-column">
+                <h4>Resources</h4><br></br>
+                <h3>Lorum Ipsum blah blah blah</h3><br></br>
+                <p>Here is some more filler text.  This should look nice hopefully.</p>
+                <a href="#" class="call-to-action">Click Here</a>
+              </div>
+              <div className="image-row-column">
+                <img class="right-column-img"
+                  src="/images/question.jpg"
+                  alt="Question"
+                  width="308px"
+                  height="200px"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="content-container">
+            <div className="container-row">
+              <div className="image-row-column">
+                <img
+                  src="/images/question.jpg"
+                  alt="Question"
+                  width="308px"
+                  height="200px"
+                />
+              </div>
+              <div className="row-column">
+                <h4>Resources</h4><br></br>
+                <h3>Lorum Ipsum blah blah blah</h3><br></br>
+                <p>Here is some more filler text.  This should look nice hopefully.</p>
+                <a href="#" class="call-to-action">Click Here</a>
+              </div>
             </div>
 
           </div>
+
         </section>
       </Layout>
     )
