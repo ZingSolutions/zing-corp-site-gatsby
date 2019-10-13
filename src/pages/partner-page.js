e510c47a4b2a907946b9c70170d2dbeb29718223
Jordan Edmunds <jordan.edmunds@prospectsoft.com>
2019-10-13 19:52:14 +0100

    tidied up bottom section, page break fail

--- src/pages/partner-page.js
+++ src/pages/partner-page.js
@@ -11,16 +11,20 @@ export default class PartnerPage extends React.Component {
           <div className="content-container">
             <div className="col-1">
               <h1>Zing Resources</h1>
+              <p>Here is some subtitle text</p>
             </div>
           </div>
         </section>
         <section>
+        <div className="content-container faded-title">
+           <span>Help me please Paul, I need this to have less margin/padding</span>
+          </div>
           <div className="content-container">
             <div className="container-row">
               <div className="image-row-column">
                 <img
-                  src="/images/question.jpg"
-                  alt="Question"
+                  src="/images/solutions.png"
+                  alt="Solutions"
                   max-width="308px"
                   height="200px"
                 />
@@ -39,13 +43,13 @@ export default class PartnerPage extends React.Component {
               <div className="row-column">
                 <h4>About Us</h4><br></br>
                 <h3>How do we differentiate?</h3><br></br>
-                <p>Here is some more filler text.  This should look nice hopefully.</p>
-                <a href="#" class="call-to-action">Click Here</a>
+                <p>Zing is a UK based Twilio Gold Consulting Partner. We have recently fully aligned ourselves to build all solutions around the Twilio portfolio and see this as central to our ambitious growth plans</p>
+                <a href="#" class="call-to-action">Find Out More</a>
               </div>
               <div className="image-row-column">
                 <img class="right-column-img"
-                  src="/images/question.jpg"
-                  alt="Question"
+                  src="/images/integration.png"
+                  alt="Integration"
                   max-width="308px"
                   height="200px"
                 />
@@ -56,8 +60,8 @@ export default class PartnerPage extends React.Component {
             <div className="container-row">
               <div className="image-row-column">
                 <img
-                  src="/images/question.jpg"
-                  alt="Question"
+                  src="/images/services.png"
+                  alt="Services"
                   max-width="308px"
                   height="200px"
                 />
@@ -65,80 +69,70 @@ export default class PartnerPage extends React.Component {
               <div className="row-column">
                 <h4>Services</h4><br></br>
                 <h3>What services do we offer?</h3><br></br>
-                <p>Here is some more filler text.  This should look nice hopefully.</p>
-                <a href="#" class="call-to-action">Click Here</a>
+                <p>Zing is a UK based Twilio Gold Consulting Partner. We have recently fully aligned ourselves to build all solutions around the Twilio portfolio and see this as central to our ambitious growth plans</p>
+                <a href="#" class="call-to-action">Find Out More</a>
               </div>
             </div>
           </div>
 
         </section>
-        //** section added to ensure white background */
-        <section></section>
+     
         <section>
+        <div className="content-container faded-title">
+           <span>Help me please Paul, I need this to have less margin/padding</span>
+          </div>
           <div className="content-container">
             <div class="flex-row">
               <div class="row-card">
                 <img
-                   src="/images/productivity.jpg"
-                   alt="Productivity"
-                   height="70%"
-                   width="70%"
+                  src="/images/services.png"
+                  alt="services"
                 />
                 <div class="card-container">
                   Logos</div>
               </div>
               <div class="row-card">
                 <img
-                   src="/images/productivity.jpg"
-                   alt="Productivity"
-                   height="70%"
-                   width="70%"
+                   src="/images/services.png"
+                   alt="services"
                 />
                 <div class="card-container">
-                  Content in here</div>
+                  Logos</div>
               </div><div class="row-card">
                 <img
-                  src="/images/productivity.jpg"
-                  alt="Productivity"
-                  height="70%"
-                   width="70%"
-                />
-                <div class="card-container">
-                  Content in here</div>
-              </div>
-            </div>
-          </div>
-          <div className="content-container">
-            <div class="flex-row">
-              <div class="row-card">
-                <img
-                   src="/images/productivity.jpg"
-                   alt="Productivity"
-                  width="100%"
+                   src="/images/services.png"
+                   alt="services"
                 />
                 <div class="card-container">
                   Content in here</div>
               </div>
               <div class="row-card">
                 <img
-                   src="/images/productivity.jpg"
-                   alt="Productivity"
-                  width="100%"
+                  src="/images/services.png"
+                  alt="services"
                 />
                 <div class="card-container">
-                  Content in here</div>
-              </div><div class="row-card">
+                  Logos</div>
+              </div>
+              <div class="row-card">
                 <img
-                   src="/images/productivity.jpg"
-                   alt="Productivity"
-                  width="100%"
+                  src="/images/services.png"
+                  alt="services"
                 />
                 <div class="card-container">
-                  Content in here</div>
+                  Logos</div>
+              </div>
+              <div class="row-card">
+                <img
+                  src="/images/services.png"
+                  alt="services"
+                />
+                <div class="card-container">
+                  Logos</div>
               </div>
             </div>
           </div>
-        </section>
+          </section>
       </Layout>
     )
   }
